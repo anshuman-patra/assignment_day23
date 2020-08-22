286549aa97029470fad883ebb5352615471ee111
anshuman-patra <anshumanpatra845@gmail.com>
2020-08-22 11:25:46 +0530

    implementing in switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,29 +1,31 @@
 public class EmployeeWage
 {
-public static void main(String[] args)
+	public static void main(String[] args)
 	{
-int fullTime=1;
-		int empHrs=0;
+		System.out.println("Welcome to employee wage problem");
+int empHrs=0;
                 int empWage=0;
                 int wagePerHour=20;
-                int partTime=2;
-		int randomCheck=(int) (Math.random()*10)%3;
-		if(randomCheck==fullTime)
-		{
+
+int randomCheck=(int) (Math.random()*10)%3;
+switch(randomCheck)
+		     {
+
+		case 1:
+
 			System.out.println("Employee is present");
 			empHrs=8;
-			}
-              else if(randomCheck==partTime)
-		{
+        case 2:
+		  System.out.println("Employee is part time");
 			empHrs=4;
-		}
+break;
+ default:
 
-		else
-		{	
 			System.out.println("Employee is absent");
-			empHrs=0;
+				empHrs=0;
+				break;
 			}
 			empWage=wagePerHour*empHrs;
 		System.out.println("Employee daily wage:" + empWage);
 }
-}
+	}
