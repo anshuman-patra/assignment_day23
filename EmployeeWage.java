78b306098566f584180e4756d9dc9f6ddcca50a7
anshuman-patra <anshumanpatra845@gmail.com>
2020-08-22 14:05:25 +0530

    implementing in methods

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,37 +2,43 @@ public class EmployeeWage
 {
 	public static final int FULL_TIME=1;
 	public static final int PART_TIME=2;
-	public static final int EMP_RATE_PER_HOUR=20;
-	public static final int NUMBER_WORKING_DAYS=20;
-	public static void main(String[] args)
+
+	public static void calculateEmployeeWage()
 	{
 		System.out.println("Welcome to employee wage problem");
+                int empRatePerHour=20;
+	        int numberOfWorkingDays=20;
 		int empHrs=0;
-		int empWage=0;
-		int totalSalary=0;
-		int totalWorkingDays=0;
-		int totalEmpHrs=0;
-		int totalMaxEmpHrs=100;
+                int totalSalary=0;
+                int totalWorkingDays=0;
+                int totalEmpHrs=0;
+                int totalMaxEmpHrs=100;
 
-		while(totalWorkingDays<NUMBER_WORKING_DAYS && totalEmpHrs<totalMaxEmpHrs)
-		{
-			totalWorkingDays++;
-			int randomCheck=(int) (Math.random()*10)%3;
-			switch(randomCheck)
-			{
-				case FULL_TIME:
-					empHrs=8;
-					break;
-				case PART_TIME:
-					empHrs=4;
-					break;
-				default:
-					empHrs=0;
-			}
-			totalEmpHrs=totalEmpHrs+empHrs;
-		}
-		totalSalary=totalEmpHrs*EMP_RATE_PER_HOUR;
-		System.out.println("Total employee hours:" + totalEmpHrs);
-		System.out.println("Total employee wage:" + totalSalary);
+                while(totalWorkingDays<numberOfWorkingDays && totalEmpHrs<totalMaxEmpHrs)
+                {
+                        totalWorkingDays++;
+                        int randomCheck=(int) (Math.random()*10)%3;
+                        switch(randomCheck)
+                        {
+                                case FULL_TIME:
+                                        empHrs=8;
+                                        break;
+                                case PART_TIME:
+                                        empHrs=4;
+                                        break;
+                                default:
+                                        empHrs=0;
+                        }
+                        totalEmpHrs=totalEmpHrs+empHrs;
+                }
+                totalSalary=totalEmpHrs*empRatePerHour;
+                System.out.println("Total employee hours:" + totalEmpHrs);
+                System.out.println("Total employee wage:" + totalSalary);
+	}
+
+	public static void main(String[] args)
+	{
+		calculateEmployeeWage();
 	}
 }
+
